594bf5c2719526fb2f0f761271df9010e149a46b
QueueMin <okkimok123@kookmin.ac.kr>
2022-05-31 14:34:47 +0900

    stage에서 움직이는 snake의 정보를 저장하는 snake class 추가

--- src/snake.cpp
+++ src/snake.cpp
@@ -5,19 +5,45 @@ int dx[4] = {1, -1, 0, 0};  //
 
 class snake{
     private:
-        int headVector;
-        int bodyLen;
+        int headVector = 1;
+        int bodyLen = 3;
         
         int Y;
         int X;
     
     public:
+        snake::snake(int y, int x){
+            Y = y;
+            X = x;
+        }
+
+
         int getHeadVector(){
             return headVector;
         }
         
         int getBodyLen(){
             return bodyLen;
+        }
+
+        void setHeadVector(int a){
+            headVector = a;
+        }
+
+        void setY(int y){
+            Y = y;
+        }
+
+        void setX(int x){
+            X = x;
+        }
+
+        bool isDead(){
+            return bodyLen<=3;
+        }
+
+        int collideWith(int stage[][MSIZE]){
+            return stage[Y+dy[headVector]][X+dx[headVector]];
         }
 
 

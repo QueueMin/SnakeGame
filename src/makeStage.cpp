b41bc07d449adb7f222a384af4da94e5e62081b5
tjdgh970105 <71748803+tjdgh970105@users.noreply.github.com>
2022-06-05 15:10:12 +0900

    Update makeStage.cpp

--- src/makeStage.cpp
+++ src/makeStage.cpp
@@ -4,6 +4,39 @@
 #define MSIZE 21
 
 void makeStage(int stageNum, int stage[][MSIZE]){
+    #include<iostream>
+#include<fstream>
+#include<string>
+#define MSIZE 21
+using namespace std;
+void makeStage(int stageNum, int stage[][MSIZE]) {
+    string s = to_string(stageNum);
+    string t = "stage" + s + ".txt";
+    int row = sizeof(stage) / sizeof(stage[0]);
+    ifstream inputfile(t);
+
+    if (!inputfile.is_open())
+        cout << "Error opening file";
+
+    for (int r = 0; r < MSIZE; r++)
+    {
+        for (int c = 0; c < MSIZE; c++)
+        {
+            inputfile >> stage[r][c];
+        }
+    }
+
+    for (int r = 0; r < MSIZE; r++)
+    {
+        for (int c = 0; c < MSIZE; c++)
+        {
+            cout << stage[r][c] << "\t";
+        }
+        cout << endl;
+    }
+    
+}
+
     
 }
 

f88d4573e54160e1eb953d2025fcbdd6b6ff4f90
QueueMin <okkimok123@kookmin.ac.kr>
2022-06-05 17:42:10 +0900

    main debug위한 코드 정정

--- src/main.cpp
+++ src/main.cpp
@@ -10,31 +10,31 @@ int main(){
 
     int stageLoaded[MSIZE][MSIZE];
     
-    // // for debug
-    // int stageNum;
-    // std::cin >> stageNum;
-    // makeStage(stageNum, stageLoaded);
+    // for debug
+    int stageNum;
+    std::cin >> stageNum;
+    makeStage(stageNum, stageLoaded);
     
     // for (int i = 0; i < MSIZE; i++){
-    //     for (int j = 0; j < MSIZE; i++){
+    //     for (int j = 0; j < MSIZE; j++){
     //         std::cout << stageLoaded[i][j];
     //     }
     //     std::cout << "\n";
     // }
     // for debug end
 
-    // for debug
-    std::string a = "211111111111111111112100000000000000000001100000000000000500001800000000000000000007100000000000000000001100000000000000000001100000000000000000001100000000000000000001100000000000000000001100000000505050500001100000003440000000001100000000000000000001100000000000000000001100000000000000000001100000000066600000001100000000000000000001100000000000000000001100000000000000000001100000000000000000001100000000000000000001211111111111111111112";
-    for (int i = 0; i < MSIZE; i++){
-        for (int j = 0; j < MSIZE; j++){
-            char t = a[i*MSIZE+j];
-            stageLoaded[i][j] =  t - '0';
-        }
-    }
-    // for debug end
+    // // for debug
+    // std::string a = "211111111111111111112100000000000000000001100000000000000500001800000000000000000007100000000000000000001100000000000000000001100000000000000000001100000000000000000001100000000000000000001100000000505050500001100000003440000000001100000000000000000001100000000000000000001100000000000000000001100000000066600000001100000000000000000001100000000000000000001100000000000000000001100000000000000000001100000000000000000001211111111111111111112";
+    // for (int i = 0; i < MSIZE; i++){
+    //     for (int j = 0; j < MSIZE; j++){
+    //         char t = a[i*MSIZE+j];
+    //         stageLoaded[i][j] =  t - '0';
+    //     }
+    // }
+    // // for debug end
 
     // for check snake move
-    initscr();
+    // initscr();
     while (true){
         Snake s = Snake(stageLoaded);
         char key;
@@ -45,8 +45,8 @@ int main(){
         // 1. 키입력을 바탕으로 머리의 방향을 바꾼다. 입력 없으면 유지.
         // 2. 현재 머리의 방향을 기준으로 도착한 다음칸에 도착 시 일어날 변화 반영
         while (!s.isDead()){
-            // displayOnTerminal(stageLoaded);
-            displayStage(stageLoaded);
+            displayOnTerminal(stageLoaded);
+            // displayStage(stageLoaded);
             std::cin >> key;
             std::cout << "\n";
 
@@ -58,7 +58,7 @@ int main(){
 
     std::cout << "Game Over\n";
     getchar();
-    endwin();
+    // endwin();
     // for check snake move end
 
     return 0;

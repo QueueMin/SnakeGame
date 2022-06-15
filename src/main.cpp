edd29d564e5f2776b3ad21a7e3f518aff22bcdec
QueueMin <okkimok123@kookmin.ac.kr>
2022-06-15 17:26:57 +0900

    스테이지 클리어 및 게임 종료 시퀀스 추가. 디버그코드 삭제.

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,7 @@
 #include "makeStage.cpp"
 #include "snake.cpp"
 #include "displayStage.cpp"
-#include "drawRealScore.cpp"
-
-
-#include "displayOnTerminal.cpp" // for debug
+#include "drawScore.cpp"
 
 #define sleep(t) usleep(t)
 
@@ -18,13 +15,13 @@ int main(){
     initscr();
     resize_term(30, 90);
     WINDOW* realScore;
-    realScore = newwin(12, 30, 1, 48);
+    realScore = newwin(8, 20, 1, 48);
     wbkgd(realScore, 2);
     wrefresh(realScore);
 
     WINDOW* goalScore;
     init_pair(3, COLOR_YELLOW, COLOR_BLUE);
-    goalScore = newwin(12, 30, 11, 48);
+    goalScore = newwin(8, 20, 9, 48);
     wbkgd(goalScore, 3);
     wrefresh(goalScore);
 
@@ -33,17 +30,27 @@ int main(){
     noecho();
     curs_set(0);
     nodelay(stdscr, true);
+    keypad(stdscr, true);
 
     int stageLoaded[MSIZE][MSIZE];
 
     int stageNum = 1;
+    int mission[5][5] =
+        {
+        // G, +, -, Len, T
+            {0, 0, 0, 0, 0},
+            {1, 3, 3, 6, 10},
+            {3, 3, 3, 7, 10},
+            {5, 2, 2, 5, 20},
+            {0, 7, 0, 10, 60}
+        };
     makeStage(stageNum, stageLoaded);
     while (stageNum <= 4){
         Snake s = Snake(stageLoaded);
         gateManager gm = gateManager(stageLoaded);
         itemManager im = itemManager(stageLoaded);
-        scoreBoard sc = scoreBoard(10, 10, 10, 10, 10);
-        char key;
+        scoreBoard sc = scoreBoard(mission[stageNum]);
+        int vin;
 
         // snake 내의 method들이 작동해야하는 순서는
         // 오직 다음 순간만을 기준으로 생각하여 작동시킨다.
@@ -54,35 +61,66 @@ int main(){
         while (!s.isDead()){
             //input
             sleep(250000);
-            key = getch();
-            
-            //logics
+            vin = getch();
+            switch (vin){
+                case KEY_LEFT:
+                    vin = 'a'; 
+                    break;
+                case KEY_RIGHT:
+                    vin = 'd'; 
+                    break;
+                case KEY_UP:
+                    vin = 'w'; 
+                    break;
+                case KEY_DOWN:
+                    vin = 's'; 
+                    break;
+            }
 
-            s.changeHeadVector(key);
-            s.changeOnNextStep(stageLoaded, gm, im);
 
             gm.changeOnNextStep(stageLoaded);
 
             im.changeOnNextStep(stageLoaded);
 
+            s.changeHeadVector(vin);
+            s.changeOnNextStep(stageLoaded, gm, im);
+            
             sc.updateAll(s.getGateScore(), s.getGrowthScore(), s.getPoisonScore(), s.getBodyLen(), s.getLenRecordScore(), s.getTimeScore());
-
 
             //display
             move(0,0);
             displayStage(stageLoaded);
             drawRealScore(realScore, sc);
+            drawGoalScore(goalScore, sc);
+            
+            
+            flag = sc.isClearStage();
+            if (flag) break;
         }
-        getch();
         if (flag){
+            if (stageNum == 4) continue;
+            mvprintw(10, 1, "Stage clear. Get ready for next stage...");
+            refresh();
             stageNum++;
             makeStage(stageNum, stageLoaded);
             sleep(BEFORENEXTSTAGE);
             continue;
         }else{
+            nodelay(stdscr, false);
+            mvprintw(10, 4, "Game Over. Press any key to exit....");
+            getch();
             break;
         }
     }
+    if (stageNum == 5){
+        clear();
+        nodelay(stdscr, false);
+        mvprintw(10, 1, "Congratulations! All stage cleared!");
+        mvprintw(11, 5, "Press ANY key to exit....");
+        refresh();
+        getch();
+    }
+
     endwin();
     return 0;
 }

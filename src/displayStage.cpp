7aac52919583e421cf06cdaacce94f424d3b1158
QueueMin <okkimok123@kookmin.ac.kr>
2022-06-16 11:53:31 +0900

    지시사항 삭제 및 완성

--- src/displayStage.cpp
+++ src/displayStage.cpp
@@ -2,17 +2,6 @@
 
 #define MSIZE 21
 
-#define EMPTY 0  // 빈 곳은 검게
-#define WALL 1  // 벽은 하얗게
-#define IWALL 2  // I벽도 하얗게
-#define SHEAD 3 // 뱀머리는 하늘
-#define SBODY 4 // 몸은 파랑
-#define GROWTH 5 // 성장약은 빨강
-#define POISON 6 // 독은 초록
-#define GATEa 7 // GATEa는 자홍
-#define GATEb 8 // GATEb도 자홍
-
-
 void displayStage(int stage[][MSIZE]){
     start_color();
     init_pair(0,COLOR_BLACK,COLOR_BLACK);
